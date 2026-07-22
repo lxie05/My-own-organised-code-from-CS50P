932345d967ce16cd6e4afa7a6a4735a6c290ffc3
Lavitta <lavitta.x@gmail.com>
2026-07-22 22:24:17 +0100

    Update camel.py

--- week2/camel.py
+++ week2/camel.py
@@ -1,1 +1,13 @@
 
+result = ""
+string = input("Enter your camelCase")
+
+for i in string:
+    if i.isupper():
+        i = i.lower()
+        result += "_" + i
+    else:
+        result += i
+
+print (result)
+
