53e84b5505161cc303dc0244a01409b65a50d94a
Lavitta <lavitta.x@gmail.com>
2026-07-26 18:32:45 +0100

    Update coke.py

--- week2/coke.py
+++ week2/coke.py
@@ -1,1 +1,9 @@
-
+total = 0
+while total < 50:
+    coin = int(input("Insert Coin:"))
+    if coin == 5 or coin == 10 or coin == 25:
+        total += coin
+    if total < 50:
+        print("Amount Due:", 50 - total)
+    if total >= 50:
+        print("Change Owed:", total - 50)
